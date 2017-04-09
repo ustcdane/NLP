bf62adb0c4035fa7866c5b5efb684fd3bb6f4e16
ustcdane <ustcdane@gmail.com>
2017-04-09 14:17:50 +0800

    add gbk digit&letter segment
    v

--- maxProbabilitySegment/src/Segmentor.h
+++ maxProbabilitySegment/src/Segmentor.h
@@ -1,107 +1,135 @@
-#pragma once
+#ifndef _SEGMENTER_
+#define _SEGMENTER_
 
 #include <cstdio>
 #include <cmath>
-#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <tr1/unordered_map>
 
 using namespace std;
 
-#define SENTENCE_MAX_LEN (1024+1)
-#define DEBUG_INFO 1
-
 typedef int  VocabIndex;
 typedef double VocabFreq;
-typedef unordered_map<string, VocabIndex> SIMAP;// word index
-typedef unordered_map<string, VocabIndex>::iterator SIMAP_IT;
+typedef tr1::unordered_map<string, VocabIndex> SIMAP;
+typedef tr1::unordered_map<string, VocabIndex>::iterator SIMAP_IT;
 
-class Segmentor {
+class Segmentor{
 public:
-	const static double c_punish;
-	const static double c_logmin;
-	const static int    c_max;
-	const static double c_stop;
-	const static int    c_dic_maxlen;
-	const static int    c_dic_maxsize;
+	const static double c_punish = 198;
+	const static double c_logmin = 100000;
+	const static int    c_max    = (unsigned short)(-1);
+	const static double c_stop = 300;
+    const static int    c_dic_maxlen = 512;
+    const static int    c_dic_maxsize = 500000;
 
 public:
-	Segmentor(const char* dic_name):dictSize_(0), hzLongest_(0) {
-		hz_.push_back("");
-		hzFreq_.push_back(0);
-		hzLength_.push_back(0);
-
-		load_dic(dic_name);
-		calcProbability();
+	Segmentor(const char* dic_name, const char* dicwrong_name){
+        m_hash.clear(); 
+        m_hz.clear();   m_hz.push_back("");
+        m_freq.clear(); m_freq.push_back(0);
+        m_length.clear(); m_length.push_back(0);
+        m_dicsize = 0;  m_longest = 0;
+        
+        load_dic(dic_name);
+        load_dicwrong(dicwrong_name);
+        calc_rate();
 	}
-	~Segmentor() {}
+	~Segmentor(){}
 	int SegmentFast(const char* words, int len, int* r_ids);
 
 private:
-	void load_dic(const char* dicname) {
-		fstream fin(dicname, ios::in);
-		if (!fin) {
-			cerr << "ERROR: cannot open dic file " << dicname << endl;
-			exit(-1);
+    void load_dic(const char* dicname){
+        fstream fin(dicname, ios::in);
+        if (!fin){
+            cerr << "ERROR: cannot open dic file " << dicname << endl;
+            exit(-1);
+        }
+
+        string line;
+        string hz;
+        VocabFreq freq;
+
+        m_dicsize = 0;
+        while (getline(fin, line)){
+            istringstream iss(line);
+            iss >> hz >> freq;
+            if (hz.length() > c_dic_maxlen)
+                continue;
+            if (m_hash.find(hz) != m_hash.end()){
+                cerr << "ERROR: dup in dic " << hz << endl;
+                exit(-1);
+            }
+
+            ++m_dicsize;
+            m_hash[hz] = m_dicsize;
+            m_hz.push_back(hz);
+            m_length.push_back(hz.length()/2);
+            m_freq.push_back(freq);
+            m_freq[0] += freq;
+
+            if (hz.length() > m_longest)
+                m_longest = hz.length();
+        }
+
+        fin.close();
+
+        // 2012年10月24日增加 为了把错词词频加回到正确词的词频
+        m_hz_corrected = m_hz;
+    }
+
+    void load_dicwrong(const char* dicname){
+        fstream fin(dicname, ios::in);
+        if (!fin){
+            cerr << "ERROR: cannot open dic file " << dicname << endl;
+            exit(-1);
+        }
+
+        string line;
+        while (getline(fin, line)){
+            istringstream iss(line);
+            string hz_wrong, hz_corrected;
+            iss >> hz_wrong >> hz_corrected;
+            if (hz_wrong.length() == hz_corrected.length() && m_hash.find(hz_wrong) != m_hash.end()){
+                int index=m_hash[hz_wrong];
+                m_hz_corrected[index] = hz_corrected;
+                //cout << hz_wrong << " -> " << hz_corrected << endl;
+            }
+        }
+
+        fin.close();
+    }
+
+    void test_dic(){
+        for (int i = 1; i <= m_dicsize; i++){
+            cout << m_hz[i] << "\t" << m_freq[i] << endl;
+        }
+    }
+
+	void calc_rate(){
+        m_rate.resize(m_dicsize + 1);
+        m_lograte.resize(m_dicsize + 1);
+		
+		for(int id = 1; id <= m_dicsize; id++){
+			m_rate[id] = (m_freq[id]+1.0) / (m_freq[0]+m_dicsize);
+			m_lograte[id] = log(m_rate[id]) / log(0.985);
 		}
-
-		string line;
-		string hz;
-		VocabFreq freq;
-
-		dictSize_ = 0;
-		while (getline(fin, line)) {
-			istringstream iss(line);
-			iss >> hz >> freq;
-			if (hz.length() > c_dic_maxlen)
-				continue;
-			if (hzIdHash_.find(hz) != hzIdHash_.end()) {
-				cerr << "ERROR: dup in dic " << hz << endl;
-				exit(-1);
-			}
-
-			++dictSize_;
-			hzIdHash_[hz] = dictSize_;
-			hz_.push_back(hz);
-			hzLength_.push_back(hz.length() / 2);
-			hzFreq_.push_back(freq);
-			hzFreq_[0] += freq;// total freq
-
-			if (hz.length() > hzLongest_)
-				hzLongest_ = hz.length();
-		}
-
-		fin.close();
 	}
-
-	void test_dic() {
-		for (int i = 1; i <= dictSize_; i++) {
-			cout << hz_[i] << "\t" << hzFreq_[i] << endl;
-		}
-	}
-
-	void calcProbability() { // calc word probability in dict
-		hzProb_.resize(dictSize_ + 1);
-		hzLogProb_.resize(dictSize_ + 1);
-
-		for (int id = 1; id <= dictSize_; id++) {
-			hzProb_[id] = (hzFreq_[id] + 1.0) / (hzFreq_[0] + dictSize_); // add one smooth
-			hzLogProb_[id] = log(hzProb_[id]) / log(0.985);
-		}
-	}
-
+    
 public:
-	vector<string>      hz_;
-	vector<int>         hzLength_;
-private:
-	SIMAP               hzIdHash_;     // To store the dict. <word, index>
-	vector<VocabFreq>   hzFreq_;       // word freq
-	int                 dictSize_;     // dict size
-	int                 hzLongest_;   // longest length of word in dict.
-	vector<double>      hzProb_;		    // store each word probability
-	vector<double>      hzLogProb_;	    // store each word log(probability)
+    SIMAP               m_hash;     // data structures to store the dic
+    vector<string>      m_hz;
+    vector<string>      m_hz_corrected; // 加入了dic_wrong里面的信息。
+    vector<int>         m_length;
+    vector<VocabFreq>   m_freq;
+    int                 m_dicsize;     // dic size
+    int                 m_longest;  // longest length of word in dic.
+    
+	vector<double>      m_rate;		    // store each probability
+	vector<double>      m_lograte;	    // store each log(probability)
 };
+
+#endif
