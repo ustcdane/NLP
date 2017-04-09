c3381256597276027582cc78a911d2fb935b8719
ustcdane <ustcdane@gmail.com>
2017-04-09 16:13:19 +0800

    md seg add digit&letter process
    md seg add digit&letter process

--- maxProbabilitySegment/src/Segmentor.cpp
+++ maxProbabilitySegment/src/Segmentor.cpp
@@ -1,60 +1,196 @@
 #include "Segmentor.h"
 
+const double Segmentor::c_punish = 198;
+const double Segmentor::c_logmin = 100000;
+const int    Segmentor::c_max = (unsigned short)(-1);
+const double Segmentor::c_stop = 300;
+const int    Segmentor::c_dic_maxlen = 512;
+const int    Segmentor::c_dic_maxsize = 500000;
+const int    Segmentor::c_max_len = 256;
+
+
+bool isGBKHZ(unsigned short wc) { // gb18030 has two char
+	unsigned char paramlow = wc & 0xFF;
+	unsigned char paramhigh = wc >> 8;
+	bool flag = false;
+	if (((paramlow >= 0xB0 && paramlow <= 0xF7) && (paramhigh >= 0xA1 && paramhigh <= 0xFE))
+		|| ((paramlow >= 0x81 && paramlow <= 0xA0) && (paramhigh >= 0x40 && paramhigh < 0xFE))
+		|| ((paramlow >= 0xAA && paramlow <= 0xFE) && (paramhigh >= 0x40 && paramhigh <= 0xA0)))
+		flag = true;
+	return flag;
+}
+
+typedef struct sentenceSegs{
+	std::string sentence_;// 句子
+	bool isZh_; 
+	sentenceSegs(const std::string &sen, bool isZh = false):sentence_(sen), isZh_(isZh){}
+}sentenceSegs_t;
+
+
+void preProcessSen(const char* word, int len, std::vector<sentenceSegs_t> &sentenceVec) {
+	std::string temp;
+	bool isGbk = true;
+	for (int i = 0; i < len;) {
+		if (isGBKHZ(*(unsigned short*)(word + i))) { // get gbk sentence
+			if (!isGbk && !temp.empty()) {
+				sentenceVec.push_back(sentenceSegs_t(temp, isGbk));
+				temp.clear();
+			}
+			isGbk = true;
+			temp.push_back(word[i]);
+			temp.push_back(word[i + 1]);
+			i += 2;
+		} // if gbk
+		else {
+			if (isGbk && !temp.empty()) {
+				sentenceVec.push_back(sentenceSegs_t(temp, isGbk));
+				temp.clear();
+			}
+			isGbk = false;
+			// ascii, only has one char
+			if (-1 < word[i] && word[i] < 127) {
+				temp.push_back(word[i]);
+				i += 1;
+			} // other
+			else {
+				temp.push_back(word[i]);
+				temp.push_back(word[i + 1]);
+				i += 2;
+			}
+		} // else not gbk
+	} // for
+	if (!temp.empty()) {
+		sentenceVec.push_back(sentenceSegs_t(temp, isGbk));
+	}
+}
+
+
 int Segmentor::SegmentFast(const char* word, int len, int* rIndexs) {
-	int pre [len+1];
-	int prob[len+1];
-    VocabIndex id_list[len+1];
-    VocabIndex id;
-	
+
+	int pre[SENTENCE_MAX_LEN];// 前驱节点
+	int prob[SENTENCE_MAX_LEN];// 记录当前最大的概率累加值(概率对数对应的是最小值)
+	VocabIndex id_list[SENTENCE_MAX_LEN];
+	VocabIndex id;
+
 	memset(pre, -1, sizeof(pre));
-	memset(prob,1000000,sizeof(prob));
+	memset(prob, 1000000, sizeof(prob));
 
 	pre[0] = 0;
 	prob[0] = 0;
-	
-	printf("----------- %s ----------\n", word);// by wangdan
-	for(int i = 0; i < len-1 ; i+= 2){
-		if (pre[i] == -1){
+#ifdef DEBUG_INFO
+	printf("----------- %s ----------\n", word);
+	printf("pos\tword\tlogProbility\tsumProb\n");
+#endif // DEBUG_INFO
+
+	for (int i = 0; i < len - 1; i += 2) {
+		if (pre[i] == -1) {
 			continue;
 		}
-		for(int j = i+2; j <= len; j+=2){
-			if (j - i > m_longest){
+		for (int j = i + 2; j <= len; j += 2) {
+			if (j - i > hzLongest_) {
 				break;
 			}
-            string tmp(word+i, j-i);
-            if (m_hash.find(tmp) != m_hash.end())
-                id = m_hash[tmp];
-            else 
-                continue;
-			int vl =(int)( c_punish + m_lograte[id]) + prob[i];
-			printf("%d\t%s\t%d\t%d\n", i, tmp.c_str(), int(m_lograte[id]), vl);// by wangdan
-			if (vl < prob[j]){
+
+			std::string tmp(word + i, j - i);
+			if (hzIdHash_.find(tmp) != hzIdHash_.end())
+				id = hzIdHash_[tmp];
+			else
+				continue;
+			int vl = (int)(c_punish + hzLogProb_[id]) + prob[i];
+#ifdef DEBUG_INFO
+			printf("%d\t%s\t%d\t%d\n", i, tmp.c_str(), int(hzLogProb_[id]), vl);
+#endif 
+			if (vl < prob[j]) {
 				prob[j] = vl;
-				pre [j] = i;
-                id_list[j] = id;
+				pre[j] = i;
+				id_list[j] = id;
 			}
 		}
 	}
-    int count = 0;
-	for(int i = len; i > 0; ){
-		if ( pre[i] == -1){
+	int count = 0;
+	for (int i = len; i > 0; ) {
+		if (pre[i] == -1) {
 			return 0;
 		}
-        rIndexs[count++] = id_list[i];
+		rIndexs[count++] = id_list[i];
 		i = pre[i];
 	}
-	
-	printf("\nsegment res:\n"); // by wangdan
-	// dump segment result by wangdan
-	int segResult[129] = {0};
-	segResult[0] = count;
-	for (int j=0; j < count; j++ )
-		segResult[j+1] = rIndexs[count-j-1];
-	
-	for (int j=0; j < count; j++ )
-		printf("%s ", m_hz_corrected[segResult[j+1]].c_str());
-	printf("\n");
 
+#ifdef DEBUG_INFO
+	// dump segment result
+	printf("\n%s\tsegment res:\n", word);
+	int segResult[129] = { 0 };
+	segResult[0] = count;
+	for (int j = 0; j < count; j++)
+		segResult[j + 1] = rIndexs[count - j - 1];
+
+	for (int j = 0; j < count; j++)
+		printf("%s ", hz_[segResult[j + 1]].c_str());
+	printf("\n--------------------------------------\n");
+
+#endif // DEBUG_INFO
 	return count;
 }
 
+bool Segmentor::Segment(const std::string &input, std::vector<std::string> &res) {
+	int ids[c_max_len] = {0};
+	int     segResult[c_max_len + 1] = {0};
+	if (input.length() > c_max_len)
+		return false;
+	int count = this->SegmentFast(input.c_str(), input.length(), ids);
+	if (count == 0)
+		return false;
+
+	segResult[0] = count;
+	for (int j = 0; j < count; j++)
+		segResult[j + 1] = ids[count - j - 1];
+
+	//check the continues single-hz number
+	int single = 0;
+	int max_single_len = 0;
+
+	for (int j = 0; j < count; j++) {
+		if (this->hzLength_[segResult[j + 1]] == 1) {
+			if (++single > max_single_len)
+				max_single_len = single;
+		}
+		else
+			single = 0;
+	}
+	if (max_single_len > 6)
+		return false;
+
+	for (int i = 0; i < segResult[0]; i++) {
+		//std::cout << this->hz_[segResult[i + 1]] << "\t";
+		res.push_back(this->hz_[segResult[i + 1]]);
+	}
+
+return true;
+}
+
+int Segmentor::SegmentWrap(const std::string &sentence, std::vector<std::string> &res) {
+	std::vector<sentenceSegs_t> segs;
+	preProcessSen(sentence.c_str(), sentence.length(), segs);
+
+#ifdef DEBUG_INFO
+	std::cout << "preProcess result:\n--------------------------------------- \n";
+	for (int i=0; i < segs.size(); ++i)
+		std::cout << segs[i].sentence_ << "\t";
+	std::cout << "\n----------------------------------------\n";
+#endif
+
+	int totalCount = 0;
+	std::vector<std::string> senVec;
+	for (int i = 0; i < segs.size(); ++i) {
+		if (!segs[i].isZh_) {
+			res.push_back(segs[i].sentence_);
+			totalCount++;
+		}
+		else if (Segment(segs[i].sentence_, senVec)) {
+			res.insert(res.end(), senVec.begin(), senVec.end());
+			totalCount += senVec.size();
+			senVec.clear();
+		}
+	}
+	return totalCount;
+}
--- maxProbabilitySegment/src/main.cpp
+++ maxProbabilitySegment/src/main.cpp
@@ -3,268 +3,52 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include "Annotate/PinyinAnnotator.h"
-#include "WordSegmentorForPyAnno/Segmentor/DicTree.h"
-#include "WordSegmentorForPyAnno/CommonSegmentor/WordSegmentor.h"
 #include "Segmentor.h"
 
 using namespace std;
-using namespace NSPinyinAnnotator;
 
-const int   c_max_len = 100;
-
-char  binPath[1024];       
-char  dirPath[1024];
+const int   c_max_len = 128;
 char  basePath[1024];
-char  dicwrongPath[1024];
-
-int     segResult[c_max_len+1];
-string  annoResult;
-double  tf;
-double  df;
-
 Segmentor*      seg;
 
-/***************************************************************************************
- *                                      SEG Funcions
- ***************************************************************************************/
-bool Segment(const char* input){
-    int ids[c_max_len];
+void init(int argc, char* argv[]) {
+	if (2 != argc) {
+		cerr << "ERROR: input parameters error" << endl;
+		cerr << "USAGE: ./seganno basePath dicwrongPath" << endl;
+		exit(-1);
+	}
 
-    if (strlen(input) > c_max_len)
-        return false;
-    int count = seg->SegmentFast(input, strlen(input), ids);
-    if (count == 0)
-        return false;
+	strcpy(basePath, argv[1]);
 
-    segResult[0] = count;
-    for (int j = 0; j < count; j++)
-        segResult[j+1] = ids[count-j-1];
-
-    //check the continues single-hz number
-    int single = 0;
-    int max_single_len = 0;
-    // feng@debug
-    //  string recovery="";
-    for (int j = 0; j < count; j++){
-        if (seg->m_length[segResult[j+1]] == 1){
-            if (++ single > max_single_len)
-                max_single_len = single;
-        }
-        else
-            single = 0;
-        // feng@debug
-        //  recovery += seg->m_hz[segResult[j+1]];
-    }
-    if (max_single_len > 6)
-        return false;
-    //if (strcmp(recovery.c_str(), input) != 0){
-    //    cerr << "Segment Error: " << input << " " << recovery << endl;
-    //    return false;
-    //}
-
-    return true;
+	seg = new Segmentor(basePath);
 }
 
-
-/***************************************************************************************
- *                                      ANNO Funcions
- ***************************************************************************************/
-bool Annotate(const char* input){
-    AnnotateResult annoresult[128];
-    bool isexisted = false;
-    int r = 0;
-    int i = 0;
-    int maxid = -1;
-    float maxprob = 0;
-    char buf[1024];
-
-    r = PinyinAnnotator::instance()->annotateWordBySegmentor(input, annoresult, 128, isexisted);
-    if ( r <= 0 )
-        return false;
-
-    maxid = -1;
-    maxprob = 0;
-    for (int j = 0 ; j < r ; ++j ) {
-        if ( annoresult[j].power > maxprob ) {
-            maxid = j;
-            maxprob = annoresult[j].power;
-        }
-        //@feng:debug
-        //cout << annoresult[j].str << endl;
-        //cout << annoresult[j].power << endl;
-    }
-    // feng@bug fixed: when r == 128 and all results' power == 0, error occured
-    if (maxid == -1)
-        return false;
-
-    EncodingConvertor::getInstance()->sbc2dbc(annoresult[maxid].str.c_str(), buf, 1024);
-    annoResult = buf;
-
-    int hz_count=0;
-    int py_count=0;
-    int pair=0; 
-    bool py_flag=false;
-    //char tmp[1024];
-    //char tmp_py[1024];
-    for (int i = 0; i < strlen(buf);){
-        if (buf[i] < 'a' || buf[i] > 'z'){
-            i += 2;
-            //tmp[hz_count++] = buf[i++];
-            //tmp[hz_count++] = buf[i++];
-            py_flag = false;
-        }
-        else {
-            i++;
-            //tmp_py[py_count++] = buf[i++];
-            if (!py_flag){
-                pair++;
-                py_flag = true;
-            }
-        }
-        
-    }
-    //tmp[hz_count]='\0';
-    //tmp_py[py_count]='\0';
-    //if (strcmp(tmp, input) != 0 || pair * 2 != strlen(input)){
-    if (pair * 2 != strlen(input)){
-        cerr << "Annotate Error: " << input << " " << pair << endl;
-        return false;
-    }
-
-    return true;
+void deinit() {
+	delete seg;
 }
 
-/***************************************************************************************
- *                                      AUX Funcions
- ***************************************************************************************/
-void init(int argc, char* argv[]){
-    if ( 5 != argc ) {
-        cerr << "ERROR: input parameters error" << endl;
-        cerr << "USAGE: ./seganno binPath dirPath basePath dicwrongPath" << endl;
-        exit(-1);
-    }
-
-    strcpy(binPath, argv[1]);
-    strcpy(dirPath, argv[2]);
-    strcpy(basePath, argv[3]);
-    strcpy(dicwrongPath, argv[4]);
-
-    seg = new Segmentor(basePath, dicwrongPath);
-    if (0 != PinyinAnnotator::instance()->open(argv[1], argv[2], NULL, NULL, true)) {
-        cerr << "ERROR: annotator init failed.." << endl;
-        exit(-1);
-    }
-
-}
-
-void deinit(){
-    PinyinAnnotator::instance()->clear();
-    delete seg;
-}
-
-//void dump(){/*{{{*/
-//    const char* buf = annoResult.c_str();
-//    int   len = strlen(buf);
-//    int   cur = 0;
-//
-//    int count = segResult[0];
-//    printf("%.0lf\t%.0lf\t%d", tf, df, count);
-//    for (int j = 0; j < count; j++){
-//        printf("\t");
-//        //feng@add: change output format from mix to hzpy
-//        char tmp[1024];
-//        int  letterNum = 0;
-//        //for (int k = 0; k < seg->m_length[segResult[j+1]]; k++){
-//        for (int k = 0; cur < len && k < seg->m_length[segResult[j+1]]; k++){
-//            printf("%c", buf[cur++]);
-//            printf("%c", buf[cur++]);
-//            while (cur < len && buf[cur] >= 'a' && buf[cur] <='z')
-//                //fprintf(f, "%c", buf[cur++]);
-//                tmp[letterNum++] = buf[cur++];
-//            tmp[letterNum++]='\'';
-//        }
-//        tmp[letterNum] = '\0';
-//        printf("%s", tmp);
-//    }
-//    printf("\n");
-//}
-/*}}}*/
-
-void dump(){
-    const char* buf = annoResult.c_str();
-    int   len = strlen(buf);
-    int   cur = 0;
-
-    int count = segResult[0];
-    printf("%.0lf\t%.0lf\t%d", tf, df, count);
-    for (int j = 0; j < count; j++){
-        printf("\t");
-        //feng@add: change output format from mix to hzpy
-        char tmp[1024];
-        int  letterNum = 0;
-        printf("%s", seg->m_hz_corrected[segResult[j+1]].c_str());
-        for (int k = 0; cur < len && k < seg->m_length[segResult[j+1]]; k++){
-            //printf("%c", buf[cur++]);
-            //printf("%c", buf[cur++]);
-            cur+=2;
-            while (cur < len && buf[cur] >= 'a' && buf[cur] <='z')
-                //fprintf(f, "%c", buf[cur++]);
-                tmp[letterNum++] = buf[cur++];
-            tmp[letterNum++]='\'';
-        }
-        tmp[letterNum] = '\0';
-        printf("%s", tmp);
-    }
-    printf("\n");
-}
-
-void dump_segment(){
-    for (int i = 0; i < segResult[0]; i++){
-        cout << seg->m_hz_corrected[segResult[i+1]]  << "\t";
-    }
-    cout << endl;
-}
-
-void dump_annotate(){
-    cout << annoResult << endl;
-
-}
-
-/***************************************************************************************
- *                                      MAIN Funcions
- ***************************************************************************************/
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    init(argc, argv);
+	init(argc, argv);
 
-    //char phrase[100000];
-    string line, phrase;
-    int count=0;
-    while (getline(cin, line)){
-        istringstream iss(line);
-        iss >> phrase >> tf >> df;
-        
-        // feng@debug
-        //cout << line << " ";
-        //cout << ++count << "\n";
-        
-        if (!Segment(phrase.c_str()))
-            continue;
- ///       if (!Annotate(phrase.c_str()))
-            continue;
- ///       dump();
+	string line, sentence;
+	int count = 0;
+	while (getline(cin, line)) {
+		istringstream iss(line);
+		iss >> sentence;
+		std::vector<std::string> res;
+		if (!seg->SegmentWrap(sentence, res))
+			continue;
+		std::cout << sentence << "\tsegment result:\n";
+		for (int i=0 ; i < res.size(); ++i) {
+			std::cout << res[i] << "\t";
+		}
+		std::cout << "\n";
+	}
 
-        //if (segResult[0] >= 48){
-        //    cout << line << endl;
-        //    dump();
-        //}
-        //if (++count % 1000000 == 0)
-        //    cerr << "lines processed: " << count << endl;
-    }
-
-    deinit();
-    return 0;
+	deinit();
+	getchar();
+	return 0;
 }
 
 
